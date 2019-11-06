0113f3b89eb4b552f4429ab1c8924366e30b1608
Mexwell <1107502296@qq.com>
2019-11-06 14:27:26 +0800

    first commit

---
+++ src/main/java/study/mexwelljay/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package study.mexwelljay.community.controller;
+
+/**
+ * @auther Jay
+ * @date 2019/11/5
+ * 模块概述：
+ **/
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
