83040d1b45281b3f994e6d840b67c93da9f5c668
Mexwell <1107502296@qq.com>
2019-11-21 13:27:54 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-## 社区（练手项目）
+## 码啾社区（练手项目）
 
 ##资料
-https://spring.io/guides/gs/serving-web-content
+[spring web文档]https://spring.io/guides/gs/serving-web-content
+[Bootstrap]https://v3.bootcss.com
+[Github OAuth]https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
 
 ##工具
-https://git.scm.com
-https://github.com/
+[git]https://git.scm.com
+[github]https://github.com
+[Visual Paradigm]https://www.visual-paradigm.com
---
+++ src/main/java/study/mexwelljay/community/controller/AuthorizeController.java
@@ -0,0 +1,45 @@
+package study.mexwelljay.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import study.mexwelljay.community.dto.AccessTokenDTO;
+import study.mexwelljay.community.dto.GithubUser;
+import study.mexwelljay.community.provider.GithubProvider;
+
+/**
+ * @auther Jay
+ * @date 2019/11/6
+ * 模块概述：
+ **/
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.url}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        System.out.println(user.getId());
+        System.out.println(user.getBio());
+        return "index";
+    }
+}
